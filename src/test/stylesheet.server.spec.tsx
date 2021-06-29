8ee7f3f15c42bad6bc47d636a9842a7bcf5528cd
Rob Gordon <rchristopher.gordon@gmail.com>
2021-06-28 22:33:20 -0400

    Rename stylesheet to sheet; fix weird git error

--- src/test/stylesheet.server.spec.tsx
+++ src/test/stylesheet.server.spec.tsx
@@ -2,7 +2,7 @@
  * @jest-environment node
  */
 
-import { mainSheet } from "../stylesheet";
+import { mainSheet } from "../sheet";
 
 describe("stylesheet", () => {
   it("should return null if no tag & not in browser", () => {
